3d946ee31cc4fab2707edaa2aae003b05e26f400
2014100890 <31299657+2014100890@users.noreply.github.com>
2021-01-24 21:30:57 +0900

    check compoisite

--- 007_project/main.js
+++ 007_project/main.js
@@ -3,16 +3,22 @@ const horizontal = document.querySelector('.horizontal');
 const target = document.querySelector('.target'); 
 const tag = document.querySelector('.tag'); 
 
+addEventListener('load', ()=>{
+const targetRect = target.getBoundingClientRect()
+// console.log(targetRect);
+const targetHalfWidth = targetRect.width /2 ; 
+const targetHalfHeight = targetRect.height /2 ; 
+
 document.addEventListener('mousemove', (event)=>{
     const x = event.clientX; 
     const y = event.clientY; 
-    console.log(`${x} ${y}`); 
-
-    vertival.style.left = `${x}px`; 
-    horizontal.style.top = `${y}px`; 
-    target.style.left = `${x}px`; 
-    target.style.top = `${y}px`; 
-    tag.style.left = `${x}px`; 
-    tag.style.top = `${y}px`; 
+    
+    // composite만 발생하도록 수정 
+    vertival.style.transform = `translateX(${x}px)`; 
+    horizontal.style.transform = `translateY(${y}px)`; 
+    target.style.transform = `translate(${x-targetHalfWidth}px, ${y-targetHalfHeight}px)`; 
+    tag.style.transform = `translate(${x}px, ${y}px)`; 
     tag.innerHTML = `${x}px, ${y}px`;
+    tag.style.transform = `translate(${x+20}px, ${y+20}px)`; 
 });
+}); 
