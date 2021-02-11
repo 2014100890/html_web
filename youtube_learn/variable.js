02f0002897103b30a5763b02c81c3d81d23911e1
2014100890 <31299657+2014100890@users.noreply.github.com>
2021-02-11 20:07:54 +0900

    done : making responsive header

---
+++ youtube_learn/variable.js
@@ -0,0 +1,10 @@
+// 1.use strit added in ES5 
+// use this for valina javscript 
+'use strict'; 
+
+// 2.Variable
+// let (added in ES6)
+let name = 'ellie';
+console.log(name); 
+name = 'hello'; 
+console.log(name); 
